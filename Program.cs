37b3d2f5dbdbf6ac69cf6bdc934ca85578d1fcf8
Nicholas Shinn <35953079+NGS1112@users.noreply.github.com>
2021-11-08 16:02:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuralNetIDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
