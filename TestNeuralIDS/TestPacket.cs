4142c7ba38f0e45faefaff26feded6ec9f705dc1
Nicholas Shinn <35953079+NGS1112@users.noreply.github.com>
2022-06-29 18:23:19 -0400

    Added in unit testing capabilities via NUnit.

---
+++ TestNeuralIDS/TestPacket.cs
@@ -0,0 +1,17 @@
+﻿namespace TestNeuralIDS
+{
+    [TestFixture]
+    public class TestPacket
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestNeuralIDS/TestProgram.cs
@@ -0,0 +1,17 @@
+namespace TestNeuralIDS
+{
+    [TestFixture]
+    public class TestProgram
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
